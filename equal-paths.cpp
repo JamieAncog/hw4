2cf913cf2b737732d98a8a95d64e2c1e9a813ca5
Jamie Ancog <jamieancog@gmail.com>
2024-03-04 20:41:18 -0800

    equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,53 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+bool isLeaf(Node* node);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (root == NULL){
+        return true;
+    }
+    else if (root->left == NULL && root->right == NULL){
+        return true;
+    }
+    else if (root->left == NULL) {
+        equalPaths(root->right);
+    }
+    else if (root->right == NULL){
+        equalPaths(root->left);
+    }
+    else if (isLeaf(root->left) && isLeaf(root->right)){
+        return true;
+    }
+    else if (isLeaf(root->left) || isLeaf(root->right)){
+        return false;
+    }
+    else if (isLeaf(root->left->left) && isLeaf(root->left->left) && !isLeaf(root->right->left) && !isLeaf(root->right->right)){
+        return false;
+    }
+    else if (!isLeaf(root->left->left) && !isLeaf(root->left->left) && isLeaf(root->right->left) && isLeaf(root->right->right)){
+        return false;
+    }
+    else {
+        equalPaths(root->left);
+        equalPaths(root->right);
+    }
+    return true;
 
 }
+
+bool isLeaf(Node* node){
+    if (node == NULL){
+        return true;
+    }
+    else if (node->left == NULL && node->right == NULL){
+        return true;
+    }
+    else { 
+        return false; 
+    }
+}
+
 
