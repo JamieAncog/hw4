68fa471deea45b68db2275a7e651f43e090d3efd
Jamie Ancog <jamieancog@gmail.com>
2024-03-10 04:34:58 -0700

    bst testing

--- bst-test.cpp
+++ bst-test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <cstdlib>
 #include "bst.h"
 #include "avlbst.h"
 
@@ -48,45 +49,81 @@ using namespace std;
 }
 */
 
+void testRemove(int num){
+    cout << "REMOVING KEY: " << num << endl;
+    BinarySearchTree<int,int> bst2;
+    bst2.insert(std::make_pair(7,4));
+    bst2.insert(std::make_pair(9,4));
+    bst2.insert(std::make_pair(15,4));
+    bst2.insert(std::make_pair(2,4));
+    bst2.insert(std::make_pair(1,4));
+    bst2.insert(std::make_pair(3,4));
+    bst2.remove(num);
+    bst2.print();
+    cout << endl;
+}
+
 int main(int argc, char *argv[])
 {
     // Binary Search Tree tests
-        BinarySearchTree<char,int> bt;
-    bt.insert(std::make_pair('a',1));
-    bt.insert(std::make_pair('b',2));
-    
-    cout << "Binary Search Tree contents:" << endl;
-    for(BinarySearchTree<char,int>::iterator it = bt.begin(); it != bt.end(); ++it) {
-        cout << it->first << " " << it->second << endl;
-    }
-    if(bt.find('b') != bt.end()) {
-        cout << "Found b" << endl;
-    }
-    else {
-        cout << "Did not find b" << endl;
-    }
-    cout << "Erasing b" << endl;
-    bt.remove('b');
-    bt.print();
+    BinarySearchTree<int,int> bst;
+    bst.insert(std::make_pair(15,4));
+    bst.insert(std::make_pair(-41,4));
+    bst.insert(std::make_pair(-32,99));
+    bst.insert(std::make_pair(-39,49));
+    bst.insert(std::make_pair(73,24));
+    bst.insert(std::make_pair(138,34));
+    bst.insert(std::make_pair(-117,6));
+    bst.insert(std::make_pair(147,4));
+    bst.insert(std::make_pair(-1,80));
+    bst.insert(std::make_pair(65,90));
+    bst.insert(std::make_pair(5,23));
+    bst.insert(std::make_pair(31,4));
+    bst.insert(std::make_pair(-72,4));
+    bst.insert(std::make_pair(-50,4));
+    bst.insert(std::make_pair(49,4));
+    bst.insert(std::make_pair(35,4));
+    bst.insert(std::make_pair(79,4));
+    bst.insert(std::make_pair(-60,4));
+    bst.insert(std::make_pair(105,4));
+    bst.insert(std::make_pair(-149,4));
+    bst.insert(std::make_pair(-67,4));
+    bst.insert(std::make_pair(-68,4));
+    bst.insert(std::make_pair(134,4));
+    bst.insert(std::make_pair(119,4));
+    bst.insert(std::make_pair(58,4));
+    bst.insert(std::make_pair(121,4));
+    bst.insert(std::make_pair(-22,4));
+    bst.insert(std::make_pair(46,4));
+    bst.insert(std::make_pair(36,4));
+    bst.insert(std::make_pair(-123,4));
+    bst.print();
+    bst.remove(-50);
+    bst.remove(147);
+    bst.remove(105);
+    bst.remove(-149);
+    bst.remove(31);
+    bst.remove(-123);
+    bst.remove(-60);
+    bst.remove(65);
+    bst.remove(46);
+    bst.remove(134);
+    bst.remove(138);
+    bst.remove(-67);
+    bst.remove(121);
+    bst.remove(15);
+    bst.remove(36);
+    bst.remove(79);
+    bst.remove(-39);
+    bst.remove(-41);
+    bst.remove(49);
+    bst.remove(-68);
+    bst.remove(-117);
+    bst.remove(73);
+    bst.remove(58);
+    bst.remove(35);
+    bst.remove(5);
+    bst.print();
 
-    //BinarySearchTree<char,int> bt2;
-   // bt2.insert(std::make_pair('b',7));
-    //bt2.insert(std::make_pair('d',7));
-    //bt2.insert(std::make_pair('a',8));
-    //bt2.insert(std::make_pair('e',7));
-    //bt2.insert(std::make_pair('c',7));
-    //bt2.insert(std::make_pair('i',7));
-    //bt2.insert(std::make_pair('g',7));
-    //bt2.print();
-    //bt2.remove('d');
-    //bt2.print();
-
-    //BinarySearchTree<int, double> bst;
-	//bst.insert(std::make_pair(2, 1.0));
-	//bst.insert(std::make_pair(1, 1.0));
-	//bst.insert(std::make_pair(3, 1.0));
-    //bst.print();
-	//bst.remove(3);
-    
     return 0;
 }
